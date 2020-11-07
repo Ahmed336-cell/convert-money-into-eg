9d93e92dfabbecf47d393476caba13bc5f75c824
AhmedMaherEL <65620946+Ahmed336-cell@users.noreply.github.com>
2020-11-07 23:09:59 +0200

    Create from usd to eg.c

---
+++ from usd to eg.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  float eg;
+  float usd;
+  printf("enter money in usd");
+  scanf(" %f" ,&usd);
+   
+    eg = usd*15.69;
+  printf("in eg is %f" ,eg);
+  return 0;
+}
